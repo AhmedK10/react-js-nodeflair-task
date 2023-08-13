d08ed13b2c1b0290a8ccd1276cf91dd5085aac9f
Ahmed Khogali <ahmed.khog@outlook.com>
2023-08-13 11:16:49 +0300

    created the companies js array

--- react-nodeflair/src/components/Object.jsx
+++ react-nodeflair/src/components/Object.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Jobs = [
       {
           id: 1,
-          company: 'TATA Group',
+          company: 'Shopee',
           job: 'Backend Software Engineer',
           rating: '3.6',
           type: 'Backend',
@@ -11,30 +11,138 @@ export const Jobs = [
           loc: 'Singapore',
           salary: 'S$6000 - S$7000 / mth',
           stack: ['C++', 'Python', 'Java'],
-          logo: '../../logos/Tata-Group-logo.png'
+          logo: '../../logos/shopee.png'
       },
       {
           id: 2,
-          company: 'TATA Group',
+          company: 'TikTok',
           job: 'FullStack Software Engineer',
-          rating: '1.3',
+          rating: '4.6',
           type: 'FullStack',
           time: '20 minutes ago',
           loc: 'Japan',
-          salary: 'S$6000 - S$7000 / mth',
+          salary: 'S$7500 - S$10000 / mth',
           stack: ['C++', 'C', 'JavaScript', 'Java', 'MATLAB', 'Python', 'Java'],
-          logo: '../../logos/Tata-Group-logo.png'
+          logo: '../../logos/tiktok.png'
       },
       {
           id: 3,
-          company: 'TATA Group',
+          company: 'Accenture',
           job: 'Frontend Software Engineer',
           rating: '4.5',
           type: 'Frontend',
+          time: '3 days ago',
+          loc: 'Thailand',
+          salary: 'S$5000 - S$7500 / mth',
+          stack: ['C', 'JavaScript', 'Figma', 'React'],
+          logo: '../../logos/accenture.png'
+      },
+      {
+          id: 4,
+          company: 'Agoda',
+          job: 'Data Engineer with Background in...',
+          rating: '1.7',
+          type: 'Data',
+          time: '28 days ago',
+          loc: 'vietnam',
+          salary: 'S$5000 - S$7500 / mth',
+          stack: ['C++', 'Python', 'Java'],
+          logo: '../../logos/agoda.png'
+      },
+      {
+          id: 5,
+          company: 'ByteDance',
+          job: 'FullStack Software Engineer',
+          rating: '1.3',
+          type: 'FullStack',
+          time: '55 minutes ago',
+          loc: 'Japan',
+          salary: 'S$6500 - S$9000 / mth',
+          stack: ['C++', 'C', 'JavaScript', 'Java', 'MATLAB', 'Python', 'Java'],
+          logo: '../../logos/bytedance.png'
+      },
+      {
+          id: 6,
+          company: 'Dyson',
+          job: 'DevOps Engineer',
+          rating: '2.5',
+          type: 'DevOps',
           time: '10 minutes ago',
           loc: 'Thailand',
-          salary: 'S$6000 - S$7000 / mth',
+          salary: 'S$3000 - S$5000 / mth',
           stack: ['C', 'JavaScript', 'Java', 'MATLAB'],
-          logo: '../../logos/Tata-Group-logo.png'
+          logo: '../../logos/dyson.png'
+      },
+      {
+          id: 7,
+          company: 'GovTech',
+          job: 'Backend Software Engineer',
+          rating: '3',
+          type: 'Backend',
+          time: '12 minutes ago',
+          loc: 'Singapore',
+          salary: 'S$6500 - S$7500 / mth',
+          stack: ['C++', 'Python', 'Java'],
+          logo: '../../logos/govtech.png'
+      },
+      {
+          id: 8,
+          company: 'HCLTech',
+          job: 'Network Engineer',
+          rating: '3.9',
+          type: 'SysOps',
+          time: '15 days ago',
+          loc: 'Philippines',
+          salary: 'S$6000 - S$7000 / mth',
+          stack: ['C++', 'C', 'JavaScript', 'Java', 'MATLAB', 'Python', 'Java'],
+          logo: '../../logos/hcltech.png'
+      },
+      {
+          id: 9,
+          company: 'Manulife',
+          job: 'Frontend Software Engineer',
+          rating: '4.5',
+          type: 'Frontend',
+          time: '10 hours ago',
+          loc: 'Thailand',
+          salary: 'S$4000 - S$5000 / mth',
+          stack: ['C', 'JavaScript', 'Java', 'MATLAB'],
+          logo: '../../logos/manulife.png'
+      },
+      {
+          id: 10,
+          company: 'Shopee',
+          job: 'Backend Software Engineer',
+          rating: '3.6',
+          type: 'Backend',
+          time: '22 minutes ago',
+          loc: 'Singapore',
+          salary: 'S$6000 - S$7000 / mth',
+          stack: ['C++', 'Python', 'Java'],
+          logo: '../../logos/shopee.png'
+      },
+      {
+          id: 11,
+          company: 'Singtel',
+          job: 'FullStack Software Engineer',
+          rating: '1.5',
+          type: 'FullStack',
+          time: '20 minutes ago',
+          loc: 'Singapore',
+          salary: 'S$6000 - S$7000 / mth',
+          stack: ['C++', 'C', 'JavaScript', 'Java', 'MATLAB', 'Python', 'Java'],
+          logo: '../../logos/singtel.png'
+      },
+      {
+          id: 12,
+          company: 'TikTok',
+          job: 'Frontend Software Engineer',
+          rating: '4',
+          type: 'Frontend',
+          time: '10 minutes ago',
+          loc: 'Philippines',
+          salary: 'S$7000 - S$11000 / mth',
+          stack: ['C', 'JavaScript', 'Java', 'MATLAB'],
+          logo: '../../logos/tiktok.png'
       }
     ];
